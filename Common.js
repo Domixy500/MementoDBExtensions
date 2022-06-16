20b7bc225247fa567283ed9bf90710815300be61
Domi <Dominik@PC2013>
2022-06-16 14:00:09 +0200

    added onCreate function

--- Common.js
+++ Common.js
@@ -4,3 +4,9 @@ function CreateObject(ObjTypeName) {
 	var o = ObjTypeLibrary.create(o);
 	return o;
 }
+
+function onCreate(ObjTypeName) {
+	var ObjTypeLibrary = libByName("ObjType");
+	var Type = ObjTypeLibrary.find(ObjTypeName);
+	message(Type.field("Name"));
+}
