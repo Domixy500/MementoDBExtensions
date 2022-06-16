bdb7a3005d4cb4ad718a249db98846965fe18cd3
Domi <Dominik@PC2013>
2022-06-16 13:38:24 +0200

    Correction FieldName Obj.Id

--- InitalizeDB.js
+++ InitalizeDB.js
@@ -5,14 +5,14 @@ function InitializeDB() {
 		var Obj_Obj = CreateObject("Obj");
 		var Obj_ObjType = CreateObject("ObjType");
 		Obj_Obj.link("ObjType", Obj_ObjType);
-		Obj_ObjType.set("Id", Obj_Obj.field("Id"));
+		Obj_ObjType.set("Obj.Id", Obj_Obj.field("Id"));
 		Obj_ObjType.set("Name", "Obj");
 		Obj_ObjType.link("CreateInstances", Obj_ObjType);
 	// ObjType
 		var ObjType_Obj = CreateObject("Obj");
 		var ObjType_ObjType = CreateObject("ObjType");
 		ObjType_Obj.link("ObjType", ObjType_ObjType);
-		ObjType_ObjType.set("Id", ObjType_Obj.field("Id"));
+		ObjType_ObjType.set("Obj.Id", ObjType_Obj.field("Id"));
 		ObjType_ObjType.set("Name", "ObjType");
 		ObjType_ObjType.link("CreateInstances", Obj_ObjType);
 		ObjType_ObjType.link("CreateInstances", ObjType_ObjType);
