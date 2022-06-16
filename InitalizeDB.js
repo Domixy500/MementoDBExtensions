526338b7f708d9c4dd67e803c09ac57a8faf72b3
Domi <Dominik@PC2013>
2022-06-16 13:13:38 +0200

    CreateWithName

--- InitalizeDB.js
+++ InitalizeDB.js
@@ -1,7 +1,12 @@
 function InitializeDB() {
 	if(0 == libByName("Obj").entries().length) {
 		message("Initialize...!")
-		
+	// Obj
+		var Obj = CreateObject("Obj");
+		Obj.set("Name", "Obj")
+	// ObjType
+		var ObjType = CreateObject("ObjType");
+		ObjType.set("Name", "ObjType")
 		message("Finished!")
 	}
 	else {
