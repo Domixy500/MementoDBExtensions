ecad8d831e7e2b50dc66d84d18a11c487b0c6266
Domi <Dominik@PC2013>
2022-06-16 13:23:11 +0200

    Create Obj
    Obj, ObjType and Name for Obj

--- InitalizeDB.js
+++ InitalizeDB.js
@@ -2,11 +2,11 @@ function InitializeDB() {
 	if(0 == libByName("Obj").entries().length) {
 		message("Initialize...!")
 	// Obj
-		var Obj = CreateObject("Obj");
-		Obj.set("Name", "Obj")
+		var Obj_Obj = CreateObject("Obj");
+		var Obj_ObjType = CreateObject("ObjType");
+		Obj_ObjType.set("Name", "Obj")
 	// ObjType
-		var ObjType = CreateObject("ObjType");
-		ObjType.set("Name", "ObjType")
+		
 		message("Finished!")
 	}
 	else {
