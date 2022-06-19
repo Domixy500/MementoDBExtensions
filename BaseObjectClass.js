7b900882c268e5b1f84f8913a2e493be4431fb9f
Domixy500 <95097682+Domixy500@users.noreply.github.com>
2022-06-19 07:29:34 +0200

    Update BaseObjectClass.js

--- BaseObjectClass.js
+++ BaseObjectClass.js
@@ -1,3 +1,16 @@
-var BaseObject = function() {
-  
+function Create(typeName) {
+  var library = libByName(typeName);
+  var obj = new Object();
+  obj = library.create(obj);
+  return obj;
+}
+
+function Obj(e) {
+  this.Current = e;
+  try {
+    this.Base = this.Current.field("Obj")[0];
+  }
+  catch(err) {
+    this.Base = this.Current;
+  }
 }
